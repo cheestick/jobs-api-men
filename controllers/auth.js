e814294aa8ef572399b6fbf1f7531e571f9375b3
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-07 16:00:44 +0300

    jwt creation was refactored and moved to user model file

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,13 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
+  const token = user.createJWT()
+  // handle user duplication error in mongo db
 
-  const { _id: userId, name } = user
-
-  const token = jwt.sign({ userId, name }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  })
-
-  res.status(StatusCodes.CREATED).json({ user: { name }, token })
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -29,5 +30,14 @@ UserSchema.pre('save', async function () {
   const salt = await bcrypt.genSalt(10)
   this.password = await bcrypt.hash(this.password, salt)
 })
+
+UserSchema.methods.createJWT = function () {
+  const { _id: userId, name } = this
+  const { JWT_LIFETIME, JWT_SECRET } = process.env
+  const userPayload = { userId, name }
+  const jwtOptions = { expiresIn: JWT_LIFETIME }
+
+  return jwt.sign(userPayload, JWT_SECRET, jwtOptions)
+}
 
 module.exports = mongoose.model('User', UserSchema)
