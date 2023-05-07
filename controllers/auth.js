dc39540502fe0ee7156f4b6e7126e4bc19b4f6e4
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-07 15:36:26 +0300

    jwt was used during user registration

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,18 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors')
-
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body })
-  res.status(StatusCodes.CREATED).json({ user })
+
+  const { _id: userId, name } = user
+
+  const token = jwt.sign({ userId, name }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  })
+
+  res.status(StatusCodes.CREATED).json({ user: { name }, token })
 }
 
 const login = async (req, res) => {
