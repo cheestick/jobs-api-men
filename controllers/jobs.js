8e46a720f551712619c995c9ada43cf6dcce410c
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-07 19:02:25 +0300

    delete job was coded

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -54,7 +54,19 @@ const updateJob = async (req, res) => {
 }
 
 const deleteJob = async (req, res) => {
-  res.send('delete job')
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req
+
+  const job = await Job.findByIdAndRemove({
+    _id: jobId,
+    createdBy: userId,
+  })
+
+  if (!job) throw new NotFoundError(`No job with id ${jobId}`)
+
+  res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
