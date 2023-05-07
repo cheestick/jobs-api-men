5a5ada60d71174fd0ffe5d1af3d35182fce3190d
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-07 19:58:47 +0300

    mongodb model schema validation error handling were added

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -7,6 +7,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Somethig went wrong. Try again later',
   }
 
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(',')
+
+    customError.statusCode = StatusCodes.BAD_REQUEST
+  }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
