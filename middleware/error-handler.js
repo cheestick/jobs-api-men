2d7f743e2212a4ee90eeb242462789ef65638baf
Maksym Chystiakov <maksym.chystiakov@gmail.com>
2023-05-07 19:42:53 +0300

    monog db duplication unique index (email) error was handled and custom error handler was refactored

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,11 +1,20 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    // set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Somethig went wrong. Try again later',
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field. Please, choose another value`
+    customError.statusCode = StatusCodes.BAD_REQUEST
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
